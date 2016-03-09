bbf4b63223072a9bfa5317aa76e697b9f3f1e2bc
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2016-03-09 15:22:56 -0500

    Update for scripts

--- Kettle-GPL/ETL/Loading/ScriptRepo/Load_Study.sh
+++ Kettle-GPL/ETL/Loading/ScriptRepo/Load_Study.sh
@@ -5,9 +5,10 @@ RAW_MAPPING=''
 STUDY_ID=''
 TOP_NODE=''
 FACT_SET=''
-LOAD_REGISTRY=''
-LOAD_RAW=''
+LOAD_REGISTRY='Y'
+LOAD_RAW='N'
 
+ENVIRONMENT=$KETTLE_DEVELOPMENT
 
 
 
@@ -27,14 +28,13 @@ LOAD_RAW=''
 
 ## ADMIN Variables do not change unless specified
 ## '/scratchg/data/NAMHR4/data/datafiles/'
-ENVIRONMENT=$GRDR_DEVELOPMENT
-DATA_LOCATION='/scratch/ETL/Loading/Registries/'$STUDY_ID'/data/datafiles'
-RAW_DATA='/scratch/ETL/Loading/Registries/'$STUDY_ID'/data/raw_datafiles'
+DATA_LOCATION=$KETTLE_LOADING_DIR'Registries/'$STUDY_ID'/data/datafiles'
+RAW_DATA=$KETTLE_LOADING_DIR'/Registries/'$STUDY_ID'/data/raw_datafiles'
 
-LOGFILE='''logs/'$STUDY_ID'.log'''
+LOGFILE=$KETTLE_LOADING_DIR'logs/'$STUDY_ID'.log'
 DEBUG='Basic'
 LOAD_TYPE='I'
-SORT_DIR='/scratch/'
+SORT_DIR=$KETTLE_LOADING_DIR
 
 ## DO NOT CHANGE ANYTHING BELOW!!!!!
 export KETTLE_HOME=$ENVIRONMENT
@@ -44,15 +44,16 @@ export KETTLE_HOME=$ENVIRONMENT
 
 ## Load Study's Mapped data and GRDR Concepts
 if [ $LOAD_REGISTRY == 'Y' ];
-        then 
-	echo "Here"
-	./ScriptRepo/create_clinical_data.sh ${MAPPING_FILE} '"'${STUDY_ID}'"' '"'${SORT_DIR}'"' '"'${FACT_SET}'"' ${DATA_LOCATION} '"'${RAW_DATA}'"' '"'${LOGFILE}'"' '"'${DEBUG}'"' ${LOAD_TYPE} "${TOP_NODE}" >> "${LOGFILE}" 2>>"${LOGFILE}_ERROR.log"
+        then
+        ./ScriptRepo/create_clinical_data.sh ${MAPPING_FILE} ${STUDY_ID} "'"${SORT_DIR}"'" ${FACT_SET} ${DATA_LOCATION} ${RAW_DATA} ${LOGFILE} ${DEBUG} ${LOAD_TYPE} "${TOP_NODE}" >> "${LOGFILE}" 2>>"${LOGFILE}_ERROR.log"
 fi
 
  MAPPING_FILE=$RAW_MAPPING
-
+ DATA_LOCATION=$RAW_DATA
 ## Load Study's Raw Data
 if [ $LOAD_RAW == 'Y' ];
-        ./ScriptRepo/create_clinical_data.sh ${MAPPING_FILE} '"'${STUDY_ID}'"' '"'${SORT_DIR}'"' '"'${FACT_SET}'"' ${DATA_LOCATION} '"'${RAW_DATA}'"' '"'${LOGFILE}'"' '"'${DEBUG}'"' ${LOAD_TYPE} "${TOP_NODE}" >> "${LOGFILE}" 2>>"${LOGFILE}_ERROR.log"
+        then
+        ./ScriptRepo/create_clinical_data.sh ${MAPPING_FILE} ${STUDY_ID} "'"${SORT_DIR}"'" ${FACT_SET} ${DATA_LOCATION} ${RAW_DATA} ${LOGFILE} ${DEBUG} ${LOAD_TYPE} "${TOP_NODE}/__RAW" >> "${LOGFILE}" 2>>"${LOGFILE}_ERROR.log"
 fi
+
      
--- Kettle-GPL/ETL/Loading/generate_load_script.sh
+++ Kettle-GPL/ETL/Loading/generate_load_script.sh
@@ -1,9 +1,11 @@
 ## Generate Study
-STUDY='GECO'
+STUDY=''
+
+
 
 ## Adming Configs
-LOADING_DIR='/scratch/ETL/Loading'
+LOADING_DIR=$KETTLE_LOADING_DIR
 
 mkdir -p ${LOADING_DIR}/'Registries'/${STUDY}/{'data'/{'datafiles','raw_datafiles'},'logs'}
-cp "${LOADING_DIR}/ScriptRepo/Load_Study.sh" "${LOADING_DIR}/${STUDY}_Load.sh"
-cp "${LOADING_DIR}/ScriptRepo/resolve_db_issues.sh" "${LOADING_DIR}/${STUDY}_resolve_db_issues.sh"
+cp "ScriptRepo/Load_Study.sh" "${LOADING_DIR}/${STUDY}_Load.sh"
+cp "ScriptRepo/resolve_db_issues.sh" "${LOADING_DIR}/${STUDY}_resolve_db_issues.sh"
